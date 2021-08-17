001759a0987eb402105f57564a5226811140b586
bilalmkhalil <bilalmkhalil255@gmail.com>
2021-08-17 11:59:35 +0500

    files

---
+++ main.js
@@ -0,0 +1,35 @@
+
+// let sections = document.querySelectorAll("section");
+
+// let options = {
+// 	root: null,
+// 	treshold: 1,
+// 	rootMargin: "-1px 0px -1px 0px"
+// }
+
+// const observer = new IntersectionObserver((entries, observer) => {
+// 	entries.forEach(entry => {
+// 		if(entry.isIntersecting) {
+// 			console.log(entry);
+// 		}
+// 	});
+// }, options);
+
+// sections.forEach(section => {
+// 	observer.observe(section);
+// });
+
+let perc = [80, 80, 75, 75, 70, 70];
+
+let progressBars = document.querySelectorAll("#progress");
+let perc_indicator = document.querySelectorAll("#perc_indicator");
+
+for (let i = 0; i < progressBars.length; i++) {
+	progressBars[i].value = perc[i];
+	perc_indicator[i].innerHTML = perc[i] + "%";
+}
+
+
+progressBars.forEach((progress) => {
+	progress.value;
+})
